f434ddeea4567b877df00d6e1701c6e3eff6067f
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-08-26 21:08:50 +0100

    added drop database config

--- utils/drop-database.js
+++ utils/drop-database.js
@@ -1,3 +1,19 @@
-const envFile = '../.env.test';
+const mysql = require('mysql2');
+const path = require('path');
+
+require('dotenv').config({
+    path: path.join(__dirname, '../.env.test'),
+});
+
+const {DB_PASSWORD, DB_NAME, DB_HOST, DB_USER, DB_PORT} = process.env;
+
+const connection = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    port: DB_PORT,
+    password: DB_PASSWORD,
+});
+
+connection.query(`DROP DATABASE ${DB_NAME}`, () => connection.end());
 
 
