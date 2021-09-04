0a5a23fea838826a01d088182a8cbd388bbf107e
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-04 12:08:35 +0100

    started writing update artist test

--- tests/artist-update.test.js
+++ tests/artist-update.test.js
@@ -3,3 +3,30 @@ const request = require('supertest');
 const getDB = require('../src/services/db');
 const app = require('../src/app');
 
+describe('update artist', () => {
+    let db;
+    let artists;
+    beforeEach(async () => {
+        db = await getDB();
+        await Promise.all([
+            db.query('INSERT INTO Artist (name, genre) VALUES (?,?)', [
+              'Tame Impala',
+              'rock',  
+            ]),
+            db.query('INSERT INTO Artist (name, genre) VALUES (?,?)', [
+                'Kylie Minogue',
+                'pop',
+            ]),
+            db.query('INSERT INTO Artist(name, genre) VALUES (?,?)', [
+                'Dave Brubeck',
+                'jazz',
+            ]),
+        ]);
+[artists] = await db.query('SELECT * FROM Artist');
+    });
+afterEach(async () => {
+    await db.query('DELETE FROM Artist')
+    await db.close()
+});
+describe('/artist')
+})
