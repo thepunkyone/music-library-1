ca7eb6a052203c46085d0eda5ed43acb1ae067b2
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-05 14:01:07 +0100

    added artist delete test

---
+++ tests/artist-delete.test.js
@@ -0,0 +1,32 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const getDB = require('../src/services/db');
+const app = require('../src/app');
+
+describe('delete artist', () => {
+    let db;
+    let artists;
+    beforeEach(async () => {
+        db = await getDB();
+        await Promise.all([
+            db.query('INSERT INTO Artist (name, genre) VALUES (?,?)', [
+                'Tame Impala',
+                'rock'
+            ]),
+            db.query('INSERT INTO Artist (name, genre) VALUES (?,?)',[
+                'Kylie Minogue',
+                'pop',
+            ]),
+            db.query('INSERT INTO Artist (name, genre) VALUES (?,?)', [
+                'Dave Brubeck',
+                'jazz',
+            ]),
+        ]);
+        [artists] = await db.query ('SELECT * from Artist')
+    });
+    afterEach(async () => {
+        await db.query('DELETE FROM Artist');
+        await db.close()
+    });
+    
+})
