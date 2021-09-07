51a6d478a3c345295089e883de3feaef90ac0031
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-07 19:52:01 +0100

    some additions to the create album test

--- tests/album-create.test.js
+++ tests/album-create.test.js
@@ -15,11 +15,20 @@ describe('create album', () => {
     describe('/artist/:artistId/album', () => {
         describe('POST', () => {
             it('creates a new album in the database assosciated to an artist', async () => {
-                const res = await (await request(app).post('/artist/:artistId/album')).send({
+                const artist = artists[0];
+                const res = await (await request(app).post(`/artist/${artist.Id}/album`)).send({
                     name: 'Better',
-                    year: 2018
+                    year: 2018,
+                    artistId: artist.Id
+                    
                 });
                 expect(res.status).to.equal(201);
+                const [[albumEntries]] = await db.query(
+                    `SELECT * FROM Album WHERE artist.Id = 1`
+                );
+                expect(albumEntries.name).to.equal('Better')
+                expect(albumEntries.year).to.equal(2018)
+
             })
         })
     })
