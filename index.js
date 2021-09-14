611d0bdffc3c9808def7c0ba88d59fe271873e2d
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-14 21:37:54 +0100

    database config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const app = require('./src/app.js');
-const APP_PORT = 4000;
+// eslint-disable-next-line no-undef
+const APP_PORT = process.env.PORT || 4000;
 app.listen(APP_PORT, () => {console.log(`App is listening on ${APP_PORT}`)});
--- utils/create-database.js
+++ utils/create-database.js
@@ -11,20 +11,23 @@ require('dotenv').config({
     path: path.join(__dirname, envFile)
 });
 // destructure environment variables from process.env 
+// eslint-disable-next-line no-undef
 const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT} = process.env;
 // This asyncronous function will run before app
 const setUpDatabase = async () => {
     try {
         // connect to the database
-        const db = await mysql.createConnection ({
+        const db = CLEAR_DATABASE_URL ?
+        await mysql.createConnection(CLEAR_DATABASE_URL) :
+        await mysql.createConnection({
             host: DB_HOST,
             user: DB_USER,
             password: DB_PASSWORD,
             port: DB_PORT,
         })
         // create the database if it doesn't already exist
-        await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
-        await db.query(`USE ${DB_NAME}`);
+        !CLEAR_DATABASE_URL && await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+        !CLEAR_DATABASE_URL && await db.query(`USE ${DB_NAME}`);
         await db.query(`CREATE TABLE IF NOT EXISTS Artist (
             id INT PRIMARY KEY auto_increment,
             name VARCHAR(25),
